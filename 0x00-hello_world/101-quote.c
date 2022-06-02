f0082aa717efb98fe46b5f8d38452e88fe13fcce
JesseOlisa <jessesamuel84@gmail.com>
2022-06-02 18:42:14 +0200

    source

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,11 +5,11 @@
  *
  * Return: 1 if code is a success
 */
-int main()
+int main(void)
 {
 	fwrite(
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 
-	1, 60, stderr);
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19", 
+	1, 59, stderr);
 
 	return (1);
 }
