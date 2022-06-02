8ddf1e7aaa47c03436ca7a587c855cfb632e30c8
JesseOlisa <jessesamuel84@gmail.com>
2022-06-02 16:01:54 +0200

    init

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - This is the function entry point
+ *
+ * Return: 0 if code is a success
+*/
+int main(void)
+{
+	char printString[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", printString:);
+
+	return (0);
+}
