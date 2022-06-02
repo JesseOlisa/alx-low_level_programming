7c2bd18f7ef437448992055c1cd4da9f54d6e511
JesseOlisa <jessesamuel84@gmail.com>
2022-06-02 18:00:31 +0200

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - This is the function entry point
+ *
+ * Return: 1 if code is a success
+*/
+int main(void)
+{
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 
+	60);
+
+	return (1);
+}
