22cecf0762e14d6d16ee56346b49307433fdc37d
Jesse Olisa <jessesamuel84@gmail.com>
2022-06-02 10:08:47 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
 */
 int main(void)
 {
-	chat str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
 	fwrite(str, sizeof(char), 59, stderr);
 
 	return (1);
