8c6751b902f684cf72efdc1cf74ef9b7c983142c
JesseOlisa <jessesamuel84@gmail.com>
2022-06-03 20:04:40 +0200

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - This is the function entry point
+ *
+ * This program checks for positive and negative numbers
+ *
+ * Return: 0 if code is a success
+*/
+
+int main(void)
+{
+	int num;
+	num = 0;
+	while (num <= 10)
+	{
+		printf("%d", num);
+		printf("\n");
+	}
+
+	return (0);
+}
