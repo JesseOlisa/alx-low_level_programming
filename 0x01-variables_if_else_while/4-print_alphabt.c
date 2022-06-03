0c6c482712cb88d269ff8e58e1945af2ddd35bf5
JesseOlisa <jessesamuel84@gmail.com>
2022-06-03 19:48:53 +0200

    no e or q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+/**
+ * main - This is the function entry point
+ *
+ * This program checks for positive and negative numbers
+ *
+ * Return: 0 if code is a success
+*/
+
+int main(void)
+{
+	char letters;
+
+	for (letters = 'a'; letters <= 'z'; letters++)
+	{
+		if(letters == 'e' || letters == 'q')
+		break;
+		putchar(letters);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
