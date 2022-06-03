21d10e2c0696a96f81c768a99284591de262ae3e
JesseOlisa <jessesamuel84@gmail.com>
2022-06-03 21:01:44 +0200

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - This is the function entry point
+ *
+ * This program checks for positive and negative numbers
+ *
+ * Return: 0 if code is a success
+*/
+
+int main(void)
+{
+	char letters;
+
+	for (letters = 'z'; letters <= 'a'; letters--)
+		putchar(letters);
+
+	putchar('\n');
+
+	return (0);
+}
