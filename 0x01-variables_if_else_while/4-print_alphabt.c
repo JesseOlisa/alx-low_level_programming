280fee3200b11e24e7e233ccba2edac7618abb2e
JesseOlisa <jessesamuel84@gmail.com>
2022-06-03 19:24:29 +0200

    block

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - This is the function entry point
+ *
+ * This program checks for positive and negative numbers
+ *
+ * Return: 0 if code is a success
+*/
+
+int main(void)
+{
+	char letters;
+
+	for (letters = 'a'; letters <= 'z'; letters++)
+		putchar(letters);
+	for (letters = 'A'; letters <= 'Z'; letters++)
+		putchar(letters);
+	
+	putchar('\n');
+
+	return (0);
+}
