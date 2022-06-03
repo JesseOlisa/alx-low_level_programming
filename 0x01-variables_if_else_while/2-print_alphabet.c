c7b0558070b85f55f24fa402f7d41fae31f5e093
Jesse Olisa <jessesamuel84@gmail.com>
2022-06-03 10:08:32 -0700

    alpha

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,9 +12,11 @@ int main(void)
 {
 	char letters;
 
-	for (letters = 'a', letters <= 'z'; letters++){
+	for (letters = 'a'; letters <= 'z'; letters++){
 		putchar(letters);
 	}
+
+	putchar('\n');
 
 	return (0);
 }
