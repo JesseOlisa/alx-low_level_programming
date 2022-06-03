51adf92170dd3f7772e5e9ff53e1dda0830e9f20
JesseOlisa <jessesamuel84@gmail.com>
2022-06-03 20:42:19 +0200

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - This is the function entry point
+ *
+ * This program checks for positive and negative numbers
+ *
+ * Return: 0 if code is a success
+*/
+
+int main(void)
+{
+	int num;
+
+	num = 0;
+	do {
+		putchar('0' + num); /* This converts num to their ASCII value */
+		num++;
+	} while (num < 10);
+
+	putchar('\n');
+	return (0);
+}
