1a1453bb65476c51b339e88ed3e6302b2f9038d5
JesseOlisa <jessesamuel84@gmail.com>
2022-06-04 09:43:08 +0200

    hello

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,12 +19,10 @@ int main(void)
 	{
 		for (j = 48 ; j < 57; j++)
 		{
-			for (k = 48; k <= 57; k++)
+			for (k = i; k <= 57; k++)
 			{
 				for (l = k + 1; l <= 57; l++)
 				{
-					if(i == 57 && j== 56)
-						continue;
 					putchar(i);
 					putchar(j);
 					putchar(' ');
