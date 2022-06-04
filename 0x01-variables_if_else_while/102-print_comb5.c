2eaf34e980ef621550ecfb335a5219539dc4cd19
JesseOlisa <jessesamuel84@gmail.com>
2022-06-04 09:18:47 +0200

    init

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - This is the function entry point
+ *
+ * This program checks for positive and negative numbers
+ *
+ * Return: 0 if code is a success
+*/
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48 ; j < 57; j++)
+		{
+			putchar(i);
+			putchar(j);
+			putchar(' ');
+
+			for (k = 48; k <= 57; k++)
+			{
+				for (l = k + 1; l <= 57; l++)
+				{
+					putchar(k);
+					putchar(l);
+					if (i == 57 && j == 56 && k == 57 && l == 57)
+					{
+					break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
