caecd65f4e8f15ab126b3a61d60598130ff3247e
JesseOlisa <jessesamuel84@gmail.com>
2022-06-04 08:40:30 +0200

    comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,19 +12,23 @@ int main(void)
 {
 	int i;
 	int j;
+	int k;
 
-	for (i = 48; i < 57; i++)
+	for (i = 48; i < 56; i++)
 	{
-		for (j = i + 1; j <= 57; j++)
-		{
-			putchar(i);
-			putchar(j);
-			if (i != 56 && j != 59)
+		for (j = i + 1; j < 57; j++)
+			for (k = j + 1; k <= 57; k++)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i != 55 && j != 56 && k != 57)
+				{
+					putchar(',');
+					putchar(',');
+				}
+				
 			}
-		}
 	}
 	putchar('\n');
 	return (0);
