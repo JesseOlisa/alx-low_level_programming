ee0b2f2b62bb8ae1412145768fe8c89a34dd12cb
JesseOlisa <jessesamuel84@gmail.com>
2022-06-04 07:50:22 +0200

    comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - This is the function entry point
+ *
+ * This program checks for positive and negative numbers
+ *
+ * Return: 0 if code is a success
+*/
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i < 57; i++)
+	{
+		for (j = i + 1; j <= 57; i++)
+		{
+			putchar[i];
+			putchar[j];
+			putchar[','];
+			putchar[' '];
+		}
+	}
+	return (0)
+}
