fde38e196b3661f6138c9b6081b41e3c43bb0495
JesseOlisa <jessesamuel84@gmail.com>
2022-06-07 19:54:23 +0200

    init

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints time of the day jack bauer wasted
+ *
+ * Return: Always 1 if program is successful
+ */
+
+void times_table(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		int j;
+		int tb;
+		for (j = 0; j <= 9; j++)
+		{
+			tb = j * i;
+			_putchar(tb + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		i++;
+	}
+}
