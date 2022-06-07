5d8215580a725c02da518e95534f9de3b40b7534
JesseOlisa <jessesamuel84@gmail.com>
2022-06-08 01:58:30 +0200

    init

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,7 +22,7 @@ void print_times_table(int n)
 				{
 					_putchar(' ');
 					_putchar((tb / 100) + '0');
-					_putchar((tb / 10) % 10) + '0');
+					_putchar((tb / 10) % 10 + '0');
 					_putchar((tb % 10) + '0');
 				}
 				else if (tb > 9)
