c4a9e7094e02034b48fe2553227ea03047f62d71
JesseOlisa <jessesamuel84@gmail.com>
2022-06-07 17:23:44 +0200

    init

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,11 +3,13 @@
 /**
  * _islower - is a function that prints 1 if the alphabet is lowercase
  *
+ * c - parameter for letter
+ *
  * Return: Always 1 if program is successful
  */
 int _islower(int c)
 {
-	if(c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	return (0);
 }
