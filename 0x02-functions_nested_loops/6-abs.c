14d4ece3bc5d7be000f1de0e6afb05ada4959fc4
JesseOlisa <jessesamuel84@gmail.com>
2022-06-07 18:28:39 +0200

    init

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha - returns 1 if the alphabet is lowercase or uppercase
+ *
+ * @c: parameter for letters
+ *
+ * Return: Always 1 if program is successful
+ */
+int _abs(int)
+{
+	int n;
+	int abs;
+
+	if(n > 0)
+	{
+		abs = n;
+	}
+	else
+	{
+		abs = n * (-1);
+	}
+}
