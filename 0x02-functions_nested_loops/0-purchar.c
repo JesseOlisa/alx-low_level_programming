089b1f9cac4fae33e172352a349f46cbbd30e277
JesseOlisa <jessesamuel84@gmail.com>
2022-06-07 13:32:28 +0200

    g

---
+++ 0x02-functions_nested_loops/0-purchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - this is th function entry point
+ * 
+ * this program creates a function that prints _putchar
+ * 
+ * return 0: if the code is a success
+*/
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_purchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
