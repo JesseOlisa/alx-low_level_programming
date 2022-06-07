41f5637674a2f06e4ba994f8cc9bf2ca18d0348c
JesseOlisa <jessesamuel84@gmail.com>
2022-06-08 00:47:08 +0200

    init

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,6 +15,7 @@ void print_to_98(int n)
 		while (n <= 98)
 		{
 			printf("%d", n);
+			_putchar(' ');
 			if (n != 98)
 			{
 				_putchar(',');
@@ -28,6 +29,7 @@ void print_to_98(int n)
 		while (n >= 98)
 		{
 			printf("%d", n);
+			_putchar(' ');
 			if (n != 98)
 			{
 				_putchar(',');
