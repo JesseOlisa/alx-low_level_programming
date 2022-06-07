07bb8b2f06fce4e805df25ced89d146d64dfbe73
JesseOlisa <jessesamuel84@gmail.com>
2022-06-08 00:54:24 +0200

    betty

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,30 +10,20 @@
 
 void print_to_98(int n)
 {
-	if (n < 98)
-	{
-		while (n <= 98)
+	if (n < 98) {
+		while (n < 98)
 		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				_putchar(',');
-			}
-			_putchar(' ');
+			printf("%d, ", n);
 			n++;
 		}
 	}
-	else
+	else if (n > 98)
 	{
-		while (n >= 98)
+		while (n > 98)
 		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				_putchar(',');
-			}
-			_putchar(' ');
+			printf("%d, ", n);
 			n--;
 		}
 	}
+	printf("%d", n);
 }
