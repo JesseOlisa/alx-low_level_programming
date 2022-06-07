4a68125092de09fb65930d9073d43d6dc1ac38c6
JesseOlisa <jessesamuel84@gmail.com>
2022-06-07 16:27:17 +0200

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * main - This is the entry point of the program Prints _putchar
+ * print_alphabet is a function that prints
  *
- * This prgram uses the the _putchar too print into the standard output
  * Return: Always 0 if program is successful
  */
 
 void print_alphabet(void)
 {
 	char lowerchar;
-	for (lowerchar = 'a'; lowerchar <= 'z'; lowerchar++){
+	for (lowerchar = 'a'; lowerchar <= 'z'; lowerchar++)
+	{
 		_putchar(lowerchar);
 	}
 	_putchar('\n');
