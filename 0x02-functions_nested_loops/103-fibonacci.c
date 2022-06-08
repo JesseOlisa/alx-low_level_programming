de0690904a129fc8c8f74c4be45759edc829e242
JesseOlisa <jessesamuel84@gmail.com>
2022-06-08 12:25:38 +0200

    init

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point if function
+ *
+ * This program prints the even finabocci sequences belo2 4mill
+ * it takes the value of the current number and assigns it to upNums
+ * it updates the value of num by assign by adding previous num
+ * old num is now assigned the value of previous old num
+ * Return: Always 0 if program is successful
+ */
+
+int main(void)
+{
+	long int num, oldNum, upNums;
+	int i = 0;
+
+	num = 1;
+	oldNum = 1;
+	while (num < 4000000)
+	{
+		if (num % 2 == 0)
+		{
+			printf("%ld, ", num);
+			upNums = num;
+			num = num + oldNum;
+			oldNum = upNums;
+			i++;
+		}
+	}
+	printf("%ld\n", num);
+	return (0);
+}
