44ad31a4fb062f195aaa524e8de6cab89d4e9c60
JesseOlisa <jessesamuel84@gmail.com>
2022-06-08 12:00:11 +0200

    init

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point if function
+ *
+ * This program prints the first 50 finabocci sequences
+ * it takes the value of the current number and assigns it to upNums
+ * it updates the value of num by assign by adding previous num
+ * old num is now assigned the value of previous old num
+ * Return: Always 0 if program is successful
+ */
+
+int main(void)
+{
+	int num, oldNum, upNums, i
+
+	i = 0;
+	num = 1;
+	oldNum = 1;
+	while (i < 50)
+	{
+		printf("%d, ", num);
+		upNums = num;
+		num = num + oldNum;
+		oldNum = upNums;
+		i++;
+	}
+	printf("%d\n", num);
+	return (0);
+}
