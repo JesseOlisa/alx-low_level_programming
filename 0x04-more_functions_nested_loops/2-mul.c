07cf42a0bbbd85c4cf1506269802f5a96a5954b4
JesseOlisa <jessesamuel84@gmail.com>
2022-06-09 20:27:08 +0200

    init

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - this function mulitples two numbers
+ * @a: is the first parameter
+ * @b: is the second parameter
+ * Return: 1 if it is digit
+ *
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
