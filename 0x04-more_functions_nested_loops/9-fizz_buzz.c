55e78f2a79ce9b7786ddab99ec4ac008482e682e
JesseOlisa <jessesamuel84@gmail.com>
2022-06-10 03:31:09 +0200

    init

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,11 @@ int main(void)
 
 	while (i <= 100)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ")
+		}
+		else if (i % 3 == 0)
 		{
 			printf("Fizz ");
 		}
