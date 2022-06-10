8c5ef28e1d722b2b59f04939a7d4c5b5222c0f50
JesseOlisa <jessesamuel84@gmail.com>
2022-06-10 17:21:41 +0200

    prime

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry point of function
+ *
+ * this program prints the largest prime number of  a number
+ * Return: Always 0 if program is successful
+ */
+
+int main(void)
+{
+	int i, primef;
+
+	long long int num = 612852475143;
+	for (i = 2; i < ; i++)
+	{
+		if (num % i == 0)
+		{
+			continue;
+		}
+		else {
+			return (i);
+		}
+	}
+	printf("%d\n", i);
+}
