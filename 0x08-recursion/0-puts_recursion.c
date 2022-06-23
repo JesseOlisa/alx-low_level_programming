5ff6907244530988642ed043c9bd1d7c5f863c6f
JesseOlisa <jessesamuel84@gmail.com>
2022-06-23 06:57:02 +0200

    init

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Prints a string
- * @s: Pointer to string to print.
- *
- * Return: 0
+ * _puts_recursion - print a string
+ * @s: pointer to clear
+ * Return: No
  */
 
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		printf('%c', *s);
+		_putchar(*s);
 		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
 	}
 }
